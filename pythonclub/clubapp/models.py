42009fcb450524d8fd11898701ea66a7a9c0cf69
SekllanChenRuan <xirenchen94@gmail.com>
2019-04-23 11:10:58 -0700

    created and registered models
    Created 4 classes in models.py and registered these models in admin.py

--- pythonclub/clubapp/models.py
+++ pythonclub/clubapp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    location=models.CharField(max_length=255)
+    agenda=models.TextField()
+
+    def __str__(self):
+        return self.meetingtitle
+    
+    class Meta:
+        db_table='meeting'
+        verbose_name_plural='meetings'
+
+class MeetingMinute(models.Model):
+    meeting=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance=models.ManyToManyField(User)
+    minutestext=models.TextField()
+
+    def __str__(self):
+        return self.minutestext
+    
+    class Meta:
+        db_table='meetingminute'
+        verbose_name_plural='meetingminutes'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourceurl=models.URLField(null=True, blank=True)
+    dateentry=models.DateField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.TextField()
+
+    def __str__(self):
+        return self.resourcename
+    
+    class Meta:
+        db_table='Resource'
+        verbose_name_plural='Resources'
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    dateentry=models.DateField()
+    eventtime=models.TimeField()
+    eventdescription=models.TextField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.eventtitle
+    
+    class Meta:
+        db_table='Event'
+        verbose_name_plural='Events'
