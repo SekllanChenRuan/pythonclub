d95e3f6b21c134f3016bf421f5a9172f08537f6c
SekllanChenRuan <xirenchen94@gmail.com>
2019-05-16 12:32:25 -0700

    created a forms.py
    created two form classes, the view for the form, and the .html templates for the forms.

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     path('getResource/', views.getResource, name='resources'),
     path('getMeeting/', views.getMeeting, name='meetings'),
     path('getMettingDetails/<int:id>', views.getMeetingDetails, name='meetingdetail'),
+    path('newMeeting/', views.newMeeting, name='newmeeting'),
+    path('newResource/', views.newResource, name='newresource'),
  ]
 
